ac427eb3632f9611ffd0de650510765c4bac26e5
akrimah <akrimahasif@gmail.com>
2025-10-16 15:36:49 +0500

    hello

---
+++ robofriends/src/Hello.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import './Hello.css';
+
+class Hello extends react.Component {
+  render() {
+    return (
+    <div className='f1 tc'>
+    <h1> Hello World! </h1>
+    <p> Welcome to the Robofriends App </p>
+    </div>
+    );
+  }
+}
+
+export default Hello;
