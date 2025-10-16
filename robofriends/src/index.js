fc2f957c8f3b7a636dba7a2d478d00682f2e8589
akrimah <akrimahasif@gmail.com>
2025-10-16 11:08:48 +0500

    hooks to classes

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>yoo where is this gonna show up</h1>
     <App />
   </React.StrictMode>
 );
